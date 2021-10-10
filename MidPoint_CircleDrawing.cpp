56527007813c2a3c5453de89e576e4f69175789a
LAPTOP-0BE72HIK\afifk <afifkhanab@gmail.com>
2021-10-10 12:42:51 +0530

    Midpoint Circle Algo Added

---
+++ MidPoint_CircleDrawing.cpp
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main()
+{
+    int xc, yc, r;
+    printf("Enter center x,y for circle : ");
+    scanf("%d%d", &xc, &yc);
+    printf("Enter radius r of circle : ");
+    scanf("%d", &r);
+    int x = 0, y = r, p = 1 - r;
+    printf("\nK\tP\tX\tY\tPlot\tP\n");
+    int k = 0;
+    printf("%d\t%d\t%d\t%d\t(%d,%d)\t%d\n", k, 0, x, y, x + xc, y + yc, p);
+    k++;
+    while (x + 1 < y)
+    {
+        if (p < 0)
+        {
+            int X = x;
+            ++x;
+            printf("%d\t%d\t%d\t%d\t(%d,%d)\t", k, p, x, y, x + xc, y + yc);
+            p = p + 2 * X + 3;
+            printf("%d\n", p);
+        }
+        else
+        {
+            int X = x;
+            int Y = y;
+            ++x;
+            --y;
+            printf("%d\t%d\t%d\t%d\t(%d,%d)\t", k, p, x, y, x + xc, y + yc);
+            p = p + 2 * X - 2 * Y + 5;
+            printf("%d\n", p);
+        }
+        k++;
+    }
+}
